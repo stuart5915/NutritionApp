6ac52f5c5f66add6a772cb4eb304ea6be20948e6
stuart5915 <48892812+stuart5915@users.noreply.github.com>
2019-09-03 12:04:19 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=DEMO_KEY&nutrients=205&nutrients=204&nutrients=208&nutrients=269&ndbno=01009"
+    )
+      .then(res => res.json())
+      .then(res => this.setState({ items: res }));
+  }
+
+  render() {
+    console.log(this.state.items);
+    return <div>hey</div>;
+  }
+}
+
+export default App;
